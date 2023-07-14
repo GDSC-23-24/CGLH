e929713c75d7200bf3567b21ad2f98cea1a63bda
채승지 <cotmdwl1405@gmail.com>
2023-07-14 15:59:25 +0900

    feat: 로그인 개발

--- back-end/CGLH/src/main/java/com/gdsc/CGLH/controller/UserController.java
+++ back-end/CGLH/src/main/java/com/gdsc/CGLH/controller/UserController.java
@@ -6,6 +6,7 @@ import com.gdsc.CGLH.dto.role.Role;
 import com.gdsc.CGLH.service.UserService;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
@@ -20,7 +21,7 @@ import javax.servlet.http.HttpSession;
 
 @Slf4j
 @RestController
-//@RequestMapping("/api/member")
+@RequestMapping("/api/member")
 @AllArgsConstructor
 @ComponentScan
 public class UserController {
@@ -28,36 +29,31 @@ public class UserController {
 
 
     /**
-     *  회원가입
+     * 회원가입
      */
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@RequestBody UserJoinFormDto user) {
-        try {
-            userService.register(user);
-            return ResponseEntity.ok("사용자 등록이 완료되었습니다!");
-        } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+
+        userService.register(user);
+        return ResponseEntity.ok("사용자 등록이 완료되었습니다!");
     }
 
     /**
      * 로그인
      */
-    @PostMapping("/api//login")
-    public ResponseEntity<?> login(@RequestBody UserLoginDto userLoginDto, HttpSession session){
-        try{
-            UserDto loginUser = userService.login(userLoginDto.getLoginId(), userLoginDto.getPassword());
-            session.setAttribute("user", loginUser);
-            log.info("데이터 확인: " + loginUser.getLoginId());
-            return ResponseEntity.ok("로그인");
-        }catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
-        }
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody UserLoginDto userLoginDto, HttpSession session) {
 
+        UserDto loginUser = userService.login(userLoginDto.getLoginId(), userLoginDto.getPassword());
+        session.setAttribute("user", loginUser);
+        log.info("데이터 확인: " + loginUser.getLoginId());
+        return ResponseEntity.ok("로그인");
     }
 
+
+
     /**
-     *  로그아웃
+     * 로그아웃
      */
     @PostMapping("/logout")
     public ResponseEntity<?> logout(HttpSession session) {
@@ -70,38 +66,34 @@ public class UserController {
      * 아이디 중복확인
      */
     @PostMapping("validate-id")
-    public ResponseEntity<?> validationId(@RequestBody UserLoginDto user){
+    public ResponseEntity<?> validationId(@RequestBody UserLoginDto user) {
         Boolean isValid = userService.validateLoginId(user.getLoginId());
-        if(!isValid) return ResponseEntity.badRequest().build();
+        if (!isValid) return ResponseEntity.badRequest().build();
         return ResponseEntity.ok(true);
     }
 
 
-    /**
-     *      후에 세션은 이런 식으로 로그인한 사용자만 사용할 수 있는 기능을 쓸 때 사용한다.
-     *
-     *     @GetMapping
-     *     public ResponseEntity<?> getProfile(HttpSession session) {
-     *         UserDto user = (UserDto) session.getAttribute("user");
-     *         if (user == null) {
-     *             return new ResponseEntity<>("No user logged in", HttpStatus.UNAUTHORIZED);
-     *         } else {
-     *             return ResponseEntity.ok(user);
-     *         }
-     *     }
-     */
+/**
+ * 후에 세션은 이런 식으로 로그인한 사용자만 사용할 수 있는 기능을 쓸 때 사용한다.
+ *
+ * @GetMapping public ResponseEntity<?> getProfile(HttpSession session) {
+ * UserDto user = (UserDto) session.getAttribute("user");
+ * if (user == null) {
+ * return new ResponseEntity<>("No user logged in", HttpStatus.UNAUTHORIZED);
+ * } else {
+ * return ResponseEntity.ok(user);
+ * }
+ * }
+ */
 
 
-    @Getter
-    @AllArgsConstructor
-    private class UserLoginDto{
-        private String loginId;
-        private String password;
-    }
-
-
-
-
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+private class UserLoginDto {
+    private String loginId;
+    private String password;
+}
 
 
 }
--- back-end/CGLH/src/main/java/com/gdsc/CGLH/dto/UserJoinFormDto.java
+++ back-end/CGLH/src/main/java/com/gdsc/CGLH/dto/UserJoinFormDto.java
@@ -3,10 +3,12 @@ package com.gdsc.CGLH.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserJoinFormDto {
     private String loginId;
     private String password;
